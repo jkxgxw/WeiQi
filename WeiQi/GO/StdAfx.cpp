03f4a663f60c4615fd6b557d9511d0d4c156fdce
jkxgxw <jkxgxw@sina.com>
2015-09-07 20:22:19 +0800

    clear dirt file

--- WeiQi/GO/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	I_GO.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
